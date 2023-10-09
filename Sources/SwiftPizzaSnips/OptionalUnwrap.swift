cb44a7998eb5a8eddfe88dc9cd5cf1e0b981782d
Michael Redig <mredig@gmail.com>
2023-10-09 15:10:29 -0500

    (refactor) include line and file in optional unwrap throw

--- Sources/SwiftPizzaSnips/OptionalUnwrap.swift
+++ Sources/SwiftPizzaSnips/OptionalUnwrap.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 public extension Optional {
-	func unwrap() throws -> Wrapped {
+	func unwrap(line: Int = #line, file: String = #file) throws -> Wrapped {
 		guard case .some(let wrapped) = self else {
-			throw OptionalError.nilValue(ofType: Wrapped.self)
+			throw OptionalError.nilValue(ofType: Wrapped.self, line: line, file: file)
 		}
 		return wrapped
 	}
 
 	enum OptionalError: Error {
-		case nilValue(ofType: Wrapped.Type)
+		case nilValue(ofType: Wrapped.Type, line: Int, file: String)
 	}
 }
