16b610361ada276e3a4774244972e72137f69b26
Michael Redig <mredig@gmail.com>
2023-10-20 16:10:27 -0500

    (refactor) debug output for errors

--- Sources/SwiftPizzaSnips/OptionalUnwrap.swift
+++ Sources/SwiftPizzaSnips/OptionalUnwrap.swift
@@ -8,7 +8,25 @@ public extension Optional {
 		return wrapped
 	}
 
+	// sourcery:localizedError
 	enum OptionalError: Error {
 		case nilValue(ofType: Wrapped.Type, line: Int, file: String)
 	}
 }
+
+extension Optional.OptionalError: CustomDebugStringConvertible, LocalizedError {
+	public var debugDescription: String {
+		switch self {
+		case .nilValue(let type, _, _): "OptionalError.nilValue of \(type)"
+		}
+	}
+
+	public var errorDescription: String? { debugDescription }
+
+	public var failureReason: String? { debugDescription }
+
+	public var helpAnchor: String? { debugDescription }
+
+	public var recoverySuggestion: String? { debugDescription }
+}
+
--- Sources/SwiftPizzaSnips/SimpleError.swift
+++ Sources/SwiftPizzaSnips/SimpleError.swift
@@ -1,7 +1,21 @@
+import Foundation
+
 public struct SimpleError: Error {
 	public let message: String
 
 	public init(message: String) {
 		self.message = message
 	}
+}
+
+extension SimpleError: CustomDebugStringConvertible, LocalizedError {
+	public var debugDescription: String { "SimpleError: '\(message)'" }
+
+	public var errorDescription: String? { debugDescription }
+
+	public var failureReason: String? { debugDescription }
+
+	public var helpAnchor: String? { debugDescription }
+
+	public var recoverySuggestion: String? { debugDescription }
 }
