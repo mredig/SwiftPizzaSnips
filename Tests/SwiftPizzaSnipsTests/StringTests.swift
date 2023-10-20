e1f3150800b4fbd7399f2e064034659d4a9f60a7
Michael Redig <mredig@gmail.com>
2023-10-20 14:32:57 -0500

    (refactor) organize with creating a scratchpad

--- Tests/SwiftPizzaSnipsTests/StringTests.swift
+++ Tests/SwiftPizzaSnipsTests/StringTests.swift
@@ -25,46 +25,4 @@ final class StringRandomGenerationTests: XCTestCase {
 		XCTAssertEqual(genAClean, "")
 		XCTAssertEqual(genBClean, "")
 	}
-
-
-	func characterSetBitmapExperimentation() throws {
-		let a: CharacterSet = .urlHostAllowed
-
-		var accumulator = ""
-		var str = ""
-		let bitmap = a.bitmapRepresentation
-		let mask: UInt8 = 1
-		for (byteIndex, byte) in bitmap.enumerated() {
-			guard byteIndex < 8192 else { break }
-			let bitOffset = byteIndex * 8
-			for offset in 0..<8 {
-				let base = (byte >> offset) & mask
-				if base > 0 {
-					let totalBitOffset = bitOffset + offset
-					guard
-						let scalar = UnicodeScalar(totalBitOffset)
-					else { fatalError("Invalid value") }
-					let character = Character(scalar)
-					accumulator.append(character)
-					if accumulator.count.isMultiple(of: 40) {
-						str.append(accumulator)
-						str.append("\n")
-						accumulator = ""
-					}
-//					print(character)
-				}
-			}
-		}
-		str.append(accumulator)
-		print(str)
-
-		_ = Unicode.Scalar(0x10000)
-
-		var bSet = CharacterSet(charactersIn: str)
-		bSet.remove(charactersIn: "\n")
-		let bData = bSet.bitmapRepresentation
-
-		print(bData[..<8192] == bitmap[..<8192])
-	}
-
 }
