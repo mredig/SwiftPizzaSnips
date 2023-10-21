1e0265af019c55407d7dbd45463f6df948597e5e
Michael Redig <mredig@gmail.com>
2023-10-21 02:55:57 -0500

    (feat) added a sane nsmergepolicy enum for managedobjectcontext

---
+++ Sources/SwiftPizzaSnips/CoreData/NSManagedObjectContext+Fixes.swift
@@ -0,0 +1,44 @@
+import Foundation
+import CoreData
+
+extension NSManagedObjectContext {
+	/// Just a wrapper around `mergePolicy` that is much easier to use
+	public var mergeConflictResolutionPolicy: NSMergePolicyType? {
+		get {
+			guard let mergePolicy = mergePolicy as? NSMergePolicy else { return nil }
+			switch mergePolicy {
+			case NSErrorMergePolicy as? NSMergePolicy:
+				return .errorMergePolicyType
+			case NSMergeByPropertyStoreTrumpMergePolicy as? NSMergePolicy:
+				return .mergeByPropertyStoreTrumpMergePolicyType
+			case NSMergeByPropertyObjectTrumpMergePolicy as? NSMergePolicy:
+				return .mergeByPropertyObjectTrumpMergePolicyType
+			case NSOverwriteMergePolicy as? NSMergePolicy:
+				return .overwriteMergePolicyType
+			case NSRollbackMergePolicy as? NSMergePolicy:
+				return .rollbackMergePolicyType
+			default:
+				return nil
+			}
+		}
+		set {
+			guard let newValue else {
+				return
+			}
+			switch newValue {
+			case .errorMergePolicyType:
+				mergePolicy = NSErrorMergePolicy
+			case .mergeByPropertyStoreTrumpMergePolicyType:
+				mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+			case .mergeByPropertyObjectTrumpMergePolicyType:
+				mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+			case .overwriteMergePolicyType:
+				mergePolicy = NSOverwriteMergePolicy
+			case .rollbackMergePolicyType:
+				mergePolicy = NSRollbackMergePolicy
+			@unknown default:
+				print("Unknown error policy - use `mergePolicy` for now. No change made.")
+			}
+		}
+	}
+}
--- Tests/SwiftPizzaSnipsTests/CoreDataTests.swift
+++ Tests/SwiftPizzaSnipsTests/CoreDataTests.swift
@@ -3,10 +3,7 @@ import XCTest
 @testable import SwiftPizzaSnips
 
 final class CoreDataTests: XCTestCase {
-	func testCoreDataRegisterModel() throws {
-		class ExampleModel: NSManagedObject {}
-		class SecondModel: NSManagedObject {}
-
+	private func testableCoreDataStack() throws -> CoreDataStack {
 		let bundle = Bundle(for: Self.self)
 		let subBundleURL = try bundle
 			.url(forResource: "SwiftPizzaSnips_SwiftPizzaSnipsTests", withExtension: "bundle")
@@ -16,7 +13,14 @@ final class CoreDataTests: XCTestCase {
 			.unwrap()
 			.url(forResource: "Foo", withExtension: "momd")
 			.unwrap()
-		let stack = try CoreDataStack(modelURL: modelURL)
+		return try CoreDataStack(modelURL: modelURL)
+	}
+
+	func testCoreDataRegisterModel() throws {
+		class ExampleModel: NSManagedObject {}
+		class SecondModel: NSManagedObject {}
+
+		let stack = try testableCoreDataStack()
 
 		XCTAssertTrue(stack.registeredModels.isEmpty)
 		stack.registerModel(ExampleModel.self)
@@ -25,5 +29,42 @@ final class CoreDataTests: XCTestCase {
 		XCTAssertEqual(2, stack.registeredModels.count)
 		stack.registerModel(ExampleModel.self)
 		XCTAssertEqual(2, stack.registeredModels.count)
+	}
+
+	func testMergePolicySetterGetter() throws {
+		let coreDataStack = try testableCoreDataStack()
+
+		XCTAssertEqual(coreDataStack.mainContext.mergeConflictResolutionPolicy, .errorMergePolicyType)
+		XCTAssertNotEqual(
+			coreDataStack.mainContext.mergeConflictResolutionPolicy,
+			.mergeByPropertyStoreTrumpMergePolicyType
+		)
+		XCTAssertNotEqual(
+			coreDataStack.mainContext.mergeConflictResolutionPolicy,
+			.mergeByPropertyObjectTrumpMergePolicyType
+		)
+		XCTAssertNotEqual(coreDataStack.mainContext.mergeConflictResolutionPolicy, .rollbackMergePolicyType)
+		XCTAssertNotEqual(coreDataStack.mainContext.mergeConflictResolutionPolicy, .overwriteMergePolicyType)
+
+		coreDataStack.mainContext.mergeConflictResolutionPolicy = .mergeByPropertyObjectTrumpMergePolicyType
+		XCTAssertEqual(
+			coreDataStack.mainContext.mergeConflictResolutionPolicy,
+			.mergeByPropertyObjectTrumpMergePolicyType
+		)
+
+		coreDataStack.mainContext.mergeConflictResolutionPolicy = .mergeByPropertyStoreTrumpMergePolicyType
+		XCTAssertEqual(
+			coreDataStack.mainContext.mergeConflictResolutionPolicy,
+			.mergeByPropertyStoreTrumpMergePolicyType
+		)
+
+		coreDataStack.mainContext.mergeConflictResolutionPolicy = .rollbackMergePolicyType
+		XCTAssertEqual(coreDataStack.mainContext.mergeConflictResolutionPolicy, .rollbackMergePolicyType)
+
+		coreDataStack.mainContext.mergeConflictResolutionPolicy = .overwriteMergePolicyType
+		XCTAssertEqual(coreDataStack.mainContext.mergeConflictResolutionPolicy, .overwriteMergePolicyType)
+
+		coreDataStack.mainContext.mergeConflictResolutionPolicy = .errorMergePolicyType
+		XCTAssertEqual(coreDataStack.mainContext.mergeConflictResolutionPolicy, .errorMergePolicyType)
 	}
 }
