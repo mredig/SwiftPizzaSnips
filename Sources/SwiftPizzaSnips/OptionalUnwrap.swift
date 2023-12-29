d5a7ed173ad645b7fef1e00cf09e73caa4b00f5f
Michael Redig <mredig@gmail.com>
2023-12-29 00:41:28 -0600

    (feat) optional unwrap or fatal error

--- Sources/SwiftPizzaSnips/OptionalUnwrap.swift
+++ Sources/SwiftPizzaSnips/OptionalUnwrap.swift
@@ -1,10 +1,21 @@
 import Foundation
 
 public extension Optional {
+	/// Often times it's much more convenient or clean to unwrap an optional by throwing
 	func unwrap(line: Int = #line, file: String = #file) throws -> Wrapped {
 		guard case .some(let wrapped) = self else {
 			throw OptionalError.nilValue(ofType: Wrapped.self, line: line, file: file)
 		}
+		return wrapped
+	}
+
+	/// For when you are *absolutely* suere there's a value, or the state is so corrupt that it should just exit if
+	/// this value doesn't exist.
+	func unwrapOrFatalError(message: String, line: Int = #line, file: String = #file) -> Wrapped {
+		guard case .some(let wrapped) = self else {
+			fatalError("\(file):\(line) - \(message)")
+		}
+
 		return wrapped
 	}
 
--- Tests/SwiftPizzaSnipsTests/OptionalTests.swift
+++ Tests/SwiftPizzaSnipsTests/OptionalTests.swift
@@ -9,4 +9,11 @@ final class OptionalTests: XCTestCase {
 		XCTAssertNoThrow(try a.unwrap())
 		XCTAssertThrowsError(try b.unwrap())
 	}
+
+	func testOptionalOrFatalUnwraps() {
+		let a: Int? = 0
+
+		XCTAssertNotNil(a.unwrapOrFatalError(message: "There should *definitely* be a value"))
+//		XCTAssertThrowsError(try Optional<Int>.none.unwrapOrFatalError(message: "It would be great to test this, but it would crash the tests. Oh well."))
+	}
 }
