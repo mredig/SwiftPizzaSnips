63e3343db0a9eefd5cc84a0c5aeb2a963db803d1
Michael Redig <mredig@gmail.com>
2024-04-25 17:49:31 -0500

    (feat) add windows filepath test

--- Tests/SwiftPizzaSnipsTests/URL+WindowsFilepathTests.swift
+++ Tests/SwiftPizzaSnipsTests/URL+WindowsFilepathTests.swift
@@ -11,4 +11,16 @@ final class URLWindowsFilepathTests: XCTestCase {
 
 		XCTAssertEqual(url?.path(percentEncoded: false), expectedURLPath)
 	}
+
+	func testNonWindowsFilepath() throws {
+		let regularPath1 = "/foo/bar"
+		let regularPath2 = "../foo/bar"
+		let regularPath3 = "./baz"
+		let regularPath4 = "bar/foo.doc"
+
+		XCTAssertNil(URL(windowsFilepath: regularPath1))
+		XCTAssertNil(URL(windowsFilepath: regularPath2))
+		XCTAssertNil(URL(windowsFilepath: regularPath3))
+		XCTAssertNil(URL(windowsFilepath: regularPath4))
+	}
 }
