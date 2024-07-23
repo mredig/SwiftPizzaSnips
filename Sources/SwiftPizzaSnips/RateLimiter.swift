2eb783d1f3991a36b88d46dca5371b3ed97feb14
Michael Redig <mredig@gmail.com>
2024-07-23 00:04:52 -0500

    (feat) RateLimiter

---
+++ Sources/SwiftPizzaSnips/RateLimiter.swift
@@ -0,0 +1,114 @@
+import Foundation
+
+@available(macOS 10.15, *)
+public enum RateLimiter {
+	public enum Duration  {
+		case seconds(Double)
+		case milliseconds(Double)
+		case microseconds(Double)
+		case nanoseconds(UInt64)
+
+		public var nanoseconds: UInt64 {
+			switch self {
+			case .seconds(let value):
+				UInt64(abs(value) * 1_000_000_000)
+			case .milliseconds(let value):
+				UInt64(abs(value) * 1_000_000)
+			case .microseconds(let value):
+				UInt64(abs(value) * 1_000)
+			case .nanoseconds(let value):
+				value
+			}
+		}
+
+		public var seconds: Double {
+			switch self {
+			case .seconds(let value):
+				value
+			case .milliseconds(let value):
+				value / 1_000
+			case .microseconds(let value):
+				value / 1_000_000
+			case .nanoseconds(let value):
+				Double(value) / 1_000_000_000
+			}
+		}
+	}
+
+	public struct ID: RawRepresentable, Hashable, ExpressibleByStringLiteral, ExpressibleByStringInterpolation {
+		public let rawValue: String
+
+		public init(rawValue: String) {
+			self.rawValue = rawValue
+		}
+
+		public init(_ rawValue: String) {
+			self.init(rawValue: rawValue)
+		}
+
+		public init(stringLiteral value: StringLiteralType) {
+			self.init(value)
+		}
+	}
+
+	private static let debounceLock = NSLock()
+	private static var debounceTable: [ID: Task<Void, Error>] = [:]
+
+	public static func debounce(id: ID, frequency: Duration = .milliseconds(100), _ action: @escaping () -> Void) {
+		debounceLock.lock()
+		defer { debounceLock.unlock() }
+
+		debounceTable[id]?.cancel()
+		debounceTable[id] = nil
+
+		let newTask = Task {
+			try Task.checkCancellation()
+			try await Task.sleep(nanoseconds: UInt64(frequency.nanoseconds))
+			try Task.checkCancellation()
+			action()
+			debounceLock.withLock {
+				debounceTable[id] = nil
+			}
+		}
+		debounceTable[id] = newTask
+	}
+
+	private static let throttleLock = NSLock()
+	private static var throttleTimerTable: [ID: Task<Void, Error>] = [:]
+	private static var throttleActionTable: [ID: () -> Void] = [:]
+
+	public static func throttle(id: ID, frequency: Duration = .milliseconds(100), _ action: @escaping () -> Void) {
+		throttleLock.lock()
+		defer { throttleLock.unlock() }
+
+		if throttleTimerTable[id] == nil {
+			action()
+			_createThrottleTimer(id: id, frequency: frequency)
+		} else {
+			throttleActionTable[id] = action
+		}
+	}
+
+	private static func executeAction(id: ID, frequency: Duration) {
+		throttleLock.withLock {
+			if let action = throttleActionTable[id] {
+				action()
+				throttleActionTable[id] = nil
+				_createThrottleTimer(id: id, frequency: frequency)
+			} else {
+				throttleTimerTable[id] = nil
+			}
+		}
+	}
+
+	private static func _createThrottleTimer(id: ID, frequency: Duration) {
+		let newTask = Task {
+			try Task.checkCancellation()
+			try await Task.sleep(nanoseconds: frequency.nanoseconds)
+			try Task.checkCancellation()
+
+			executeAction(id: id, frequency: frequency)
+		}
+		throttleTimerTable[id] = newTask
+	}
+}
---
+++ Tests/SwiftPizzaSnipsTests/RateLimiterTests.swift
@@ -0,0 +1,77 @@
+import XCTest
+import SwiftPizzaSnips
+
+final class RateLimiterTests: XCTestCase {
+	func testDurationSeconds() throws {
+		let twoSeconds: RateLimiter.Duration = .seconds(2)
+		let twoMilliSeconds: RateLimiter.Duration = .milliseconds(2)
+		let twoMicroSeconds: RateLimiter.Duration = .microseconds(2)
+		let twoNanoSeconds: RateLimiter.Duration = .nanoseconds(2)
+
+		XCTAssertEqual(twoSeconds.seconds, 2)
+		XCTAssertEqual(twoMilliSeconds.seconds, 0.002)
+		XCTAssertEqual(twoMicroSeconds.seconds, 0.000_002)
+		XCTAssertEqual(twoNanoSeconds.seconds, 0.000_000_002)
+	}
+
+	func testDurationNanoSeconds() throws {
+		let twoSeconds: RateLimiter.Duration = .seconds(2)
+		let twoMilliSeconds: RateLimiter.Duration = .milliseconds(2)
+		let twoMicroSeconds: RateLimiter.Duration = .microseconds(2)
+		let twoNanoSeconds: RateLimiter.Duration = .nanoseconds(2)
+
+		XCTAssertEqual(twoSeconds.nanoseconds, 2_000_000_000)
+		XCTAssertEqual(twoMilliSeconds.nanoseconds, 2_000_000)
+		XCTAssertEqual(twoMicroSeconds.nanoseconds, 2_000)
+		XCTAssertEqual(twoNanoSeconds.nanoseconds, 2)
+	}
+
+	func testRateLimitDebounce() async throws {
+		let id = RateLimiter.ID("foo")
+
+		let start = Date()
+		let end = start.addingTimeInterval(0.5)
+
+		var runTimes: [Date] = []
+		var iterations = 0
+
+		while .now < end {
+			RateLimiter.debounce(id: id, frequency: .milliseconds(100)) {
+				runTimes.append(.now)
+			}
+			iterations += 1
+		}
+
+		XCTAssertTrue(runTimes.isEmpty)
+
+		try await Task.sleep(nanoseconds: RateLimiter.Duration.milliseconds(200).nanoseconds)
+		XCTAssertEqual(runTimes.count, 1)
+		print("iterations: \(iterations)")
+	}
+
+	func testRateLimitThrottle() async throws {
+		let id = RateLimiter.ID("foo")
+
+		let start = Date()
+		let end = start.addingTimeInterval(0.5)
+
+		var runTimes: [Date] = []
+		var iterations = 0
+
+		while .now < end {
+			RateLimiter.throttle(id: id, frequency: .milliseconds(400)) {
+				runTimes.append(.now)
+			}
+			iterations += 1
+		}
+
+		XCTAssertEqual(runTimes.count, 2)
+
+		try await Task.sleep(nanoseconds: RateLimiter.Duration.milliseconds(500).nanoseconds)
+		XCTAssertEqual(runTimes.count, 3)
+
+		try await Task.sleep(nanoseconds: RateLimiter.Duration.milliseconds(500).nanoseconds)
+		XCTAssertEqual(runTimes.count, 3)
+		print("iterations: \(iterations)")
+	}
+}
