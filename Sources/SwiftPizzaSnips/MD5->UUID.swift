2bcf4003be672ce359ea7dd98f84aea879ae391f
Michael Redig <mredig@gmail.com>
2025-04-24 14:54:54 -0500

    (feat) add MD5 -> UUID conversion

---
+++ Sources/SwiftPizzaSnips/MD5->UUID.swift
@@ -0,0 +1,37 @@
+import Foundation
+#if canImport(CryptoKit)
+import CryptoKit
+#elseif canImport(Crypto)
+import Crypto
+#endif
+
+#if canImport(CryptoKit) || canImport(Crypto)
+@available(macOS 10.15, iOS 13.2, tvOS 13.2, watchOS 6.1, *)
+extension UUID {
+	/// MD5 hashes and UUIDs have the same byte count. There are situations where it's convenient to exchange one for
+	/// the other (for example, providing a unique, consistent ID for a set of data that wasn't previously assigned an
+	/// ID - just don't use for cryptographically secure needs as MD5 is no longer secure)
+	/// - Parameter md5Hash: The MD5 hash in Apple's CryptoKit format
+	public init(md5Hash: Insecure.MD5Digest) {
+		let data = Data(md5Hash)
+		self.init(uuid: (
+			data[0],
+			data[1],
+			data[2],
+			data[3],
+			data[4],
+			data[5],
+			data[6],
+			data[7],
+			data[8],
+			data[9],
+			data[10],
+			data[11],
+			data[12],
+			data[13],
+			data[14],
+			data[15]
+		))
+	}
+}
+#endif
---
+++ Tests/SwiftPizzaSnipsTests/MD5->UUIDTests.swift
@@ -0,0 +1,26 @@
+import Testing
+import SwiftPizzaSnips
+import Foundation
+#if canImport(CryptoKit)
+import CryptoKit
+#elseif canImport(Crypto)
+import Crypto
+#endif
+
+#if canImport(CryptoKit) || canImport(Crypto)
+struct MD5ToUUIDTests {
+	@Test(arguments: [
+		("asdf", UUID(uuidString: "912EC803-B2CE-49E4-A541-068D495AB570")),
+		("1", UUID(uuidString: "C4CA4238-A0B9-2382-0DCC-509A6F75849B")),
+		("Never once touched my per diem. I'd go to Craft Service, get some raw veggies, bacon, Cup-A-Soup…baby, I got a stew goin'.", UUID(uuidString: "C1BCE429-C226-F123-C2C3-C5516C50DA39")),
+	])
+	func simple(inputValue: String, expectedHashedID: UUID?) async throws {
+		var hasher = Insecure.MD5()
+		hasher.update(string: inputValue)
+
+		let hash = hasher.finalize()
+		let newID = UUID(md5Hash: hash)
+		#expect(newID == expectedHashedID)
+	}
+}
+#endif
