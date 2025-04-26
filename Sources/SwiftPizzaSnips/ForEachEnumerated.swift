7ff8ff30f0d60f22ca19a03b57249a898120444a
Michael Redig <mredig@gmail.com>
2025-04-26 00:53:52 -0500

    (feat) added tests and documentation for foreachenumerated

--- Sources/SwiftPizzaSnips/ForEachEnumerated.swift
+++ Sources/SwiftPizzaSnips/ForEachEnumerated.swift
@@ -1,14 +1,52 @@
 public extension RandomAccessCollection where Index == Int {
+	/// SwiftUI's `ForEach` doesn't allow `.enumerated()` sequences because it doesn't conform to `RandomAccessCollection`.
+	///
+	/// Well, `ForEachEnumeratedSequence` does. This allows you to write code like this:
+	///
+	/// ```swift
+	///	let sequence = "abcdefghijklm".map { String($0) }
+	/// List {
+	/// 	ForEach(sequence.forEachEnumerated(), id: \.self.element) { (offset, letter) in
+	/// 		if offset != sequence.startIndex {
+	/// 			Divider()
+	/// 		}
+	/// 		Text(letter)
+	/// 	}
+	/// }
+	/// ```
+	///
+	/// This allows you to conditionally alter your layout based on the offset of the element you're on, like only putting dividers between items
+	/// in a list, avoiding a `Divider` on the very top or bottom of the `VStack`, etc.
+	///
+	/// - Returns: An enumerated sequence
 	func forEachEnumerated() -> ForEachEnumeratedSequence<Self> {
 		ForEachEnumeratedSequence(base: self)
 	}
 }
 
+/// SwiftUI's `ForEach` doesn't allow `.enumerated()` sequences because it doesn't conform to `RandomAccessCollection`.
+///
+/// Well, `ForEachEnumeratedSequence` does. This allows you to write code like this:
+///
+/// ```swift
+///	let sequence = "abcdefghijklm".map { String($0) }
+/// List {
+/// 	ForEach(sequence.forEachEnumerated(), id: \.self.element) { (offset, letter) in
+/// 		if offset != sequence.startIndex {
+/// 			Divider()
+/// 		}
+/// 		Text(letter)
+/// 	}
+/// }
+/// ```
+///
+/// This allows you to conditionally alter your layout based on the offset of the element you're on, like only putting dividers between items
+/// in a list, avoiding a `Divider` on the very top or bottom of the `VStack`, etc.
 public struct ForEachEnumeratedSequence<Base: RandomAccessCollection>: Sequence where Base.Index == Int {
 	public var base: Base
 
 	public struct Iterator: IteratorProtocol {
-		public typealias Element = (Int, Base.Element)
+		public typealias Element = (offset: Int, element: Base.Element)
 
 		private var sequence: Base
 		private var offset: Int
@@ -18,7 +56,7 @@ public struct ForEachEnumeratedSequence<Base: RandomAccessCollection>: Sequence 
 			self.offset = sequence.startIndex
 		}
 
-		public mutating func next() -> (Int, Base.Element)? {
+		public mutating func next() -> Element? {
 			guard
 				sequence.indices.contains(offset)
 			else { return nil }
@@ -39,7 +77,7 @@ extension ForEachEnumeratedSequence: Collection {
 		base.index(after: i)
 	}
 
-	public subscript(position: Base.Index) -> (Int, Base.Element) {
+	public subscript(position: Base.Index) -> Element {
 		let element = base[position]
 		return (position, element)
 	}
