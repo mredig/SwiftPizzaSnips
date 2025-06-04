a4f1160e388070d8db1868b67b776f5a3c1e903f
Michael Redig <mredig@gmail.com>
2025-06-03 20:49:10 -0500

    (feat) add test for new optional unwrap

--- Tests/SwiftPizzaSnipsTests/OptionalTests.swift
+++ Tests/SwiftPizzaSnipsTests/OptionalTests.swift
@@ -1,34 +1,54 @@
-import XCTest
+import Testing
 import SwiftPizzaSnips
 
-final class OptionalTests: XCTestCase {
-	func testOptionalUnwraps() {
+struct OptionalTests {
+	@Test func basicOptionalUnwraps() {
 		let a: Int? = 0
 		let b: Int? = nil
 
-		XCTAssertNoThrow(try a.unwrap())
-		XCTAssertThrowsError(try b.unwrap())
+		#expect(throws: Never.self, performing: {
+			try a.unwrap()
+		})
+		#expect(throws: Optional<Int>.OptionalError.self, performing: {
+			try b.unwrap()
+		})
 	}
 
-	func testOptionalOrFatalUnwraps() {
+	@Test func optionalOrFatalUnwraps() {
 		let a: Int? = 0
 
-		XCTAssertNotNil(a.unwrapOrFatalError(message: "There should *definitely* be a value"))
-//		XCTAssertThrowsError(try Optional<Int>.none.unwrapOrFatalError(message: "It would be great to test this, but it would crash the tests. Oh well."))
+		#expect(a.unwrapOrFatalError(message: "There should *definitely* be a value") == 0)
+//		#expect(try Optional<Int>.none.unwrapOrFatalError(message: "It would be great to test this, but it would crash the tests. Oh well."))
 	}
 
-	func testOptionalUnwrapAndCast() throws {
+	@Test func optionalUnwrapAndCast() throws {
 		let a: Any? = 0
 		let b: Any? = nil
 
-		XCTAssertNoThrow(try a.unwrapCast(as: Int.self))
-		XCTAssertThrowsError(try b.unwrapCast(as: Int.self))
+		#expect(throws: Never.self, performing: {
+			try a.unwrapCast(as: Int.self)
+		})
+		#expect(throws: Error.self, performing: {
+			try b.unwrapCast(as: Int.self)
+		})
 	}
 
-	func testOptionalUnwrapCastOrFatalUnwraps() {
+	@Test func optionalUnwrapCastOrFatalUnwraps() {
 		let a: Any? = 0
 
-		XCTAssertNotNil(a.unwrapCastOrFatalError(as: Int.self, message: "There should *definitely* be a value"))
-//		XCTAssertThrowsError(try Optional<Int>.none.unwrapCastOrFatalError(as: Int.self, message: "It would be great to test this, but it would crash the tests. Oh well."))
+		#expect(a.unwrapCastOrFatalError(as: Int.self, message: "There should *definitely* be a value") == 0)
+//		#expect(try Optional<Int>.none.unwrapCastOrFatalError(as: Int.self, message: "It would be great to test this, but it would crash the tests. Oh well."))
+	}
+
+	@Test func customError() async throws {
+		let a: Int? = 0
+		let b: Any? = nil
+
+		#expect(throws: Never.self, performing: {
+			try a.unwrap(or: SimpleError(message: "No int value"))
+		})
+		#expect(throws: SimpleError.self, performing: {
+			try b.unwrap(or: SimpleError(message: "No int value"))
+		})
 	}
 }
