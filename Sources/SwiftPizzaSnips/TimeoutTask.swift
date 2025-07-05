73227ecf3b6d84ec5ca3c6be15a0b4f76ae26e6c
Michael Redig <mredig@gmail.com>
2025-07-05 01:16:50 -0500

    (refactor) timeout task has unstructured option

--- Sources/SwiftPizzaSnips/TimeoutTask.swift
+++ Sources/SwiftPizzaSnips/TimeoutTask.swift
@@ -25,46 +25,51 @@ public struct TimeoutTask<Success: Sendable, Failure: TimedOutError>: Sendable, 
 	/// Same as `Task.isCancelled`
 	public var isCancelled: Bool { underlyingTask.isCancelled }
 
-	/// Same as `Task.init`
+	/// Basics are the same as `Task.init`, but `shouldUseStructuredTasks` differs.
+	///
+	/// When `shouldUseStructuredTasks` is set to `true`, that means that the whole task tree is created with structured
+	/// tasks (although, any child `Task`s of `operation` are at your mercy). This is usually the *right* strategy,
+	/// however there are some potential exceptions.
+	///
+	/// When using code that isn't async/await aware, or maybe even some code that *is*, the `Task` won't respect the
+	/// `isCancelled` state of the `Task`. The consequence of this is that, when using structured tasks, even the
+	/// cancelled branch of the tree must finish everything before the parent task can complete. So, for example, if a
+	/// task were to `usleep()` for `10` seconds, but your timeout was set to `1` second, you would still have to
+	/// wait the entire `10` seconds before learning that your task timed out. The underlying code *will still* execute
+	/// to completion regardless of this state here, but often times you don't care and just need to update UI and
+	/// consider it a write off.
+	///
+	/// Contrarily, if the `usleep()` function were called in an unstructured child task, the timeout can trigger
+	/// immediately upon firing, informing the call site that the task timed out after the expected `1` second interval.
+	///
+	/// Of note, when `shouldUseStructuredTasks` is false, it is only the `operation` closure that is passed to the
+	/// unstructured task (and therefore any children of that `Task`), but the rest of the tree remains structured.
 	public init(
 		priority: TaskPriority? = nil,
 		timeout: ContinuousClock.Instant.Duration,
+		shouldUseStructuredTasks: Bool = true,
 		@_implicitSelfCapture operation: sending @escaping @isolated(any) () async throws(Failure) -> Success
 	) {
-		self.init(detached: false, priority: priority, timeout: timeout, operation: operation)
+		self.init(
+			detached: false,
+			priority: priority,
+			timeout: timeout,
+			shouldUseStructuredTasks: shouldUseStructuredTasks,
+			operation: operation)
 	}
 
 	private init(
 		detached: Bool,
-		priority: TaskPriority? = nil,
+		priority: TaskPriority?,
 		timeout: ContinuousClock.Instant.Duration,
+		shouldUseStructuredTasks: Bool,
 		@_implicitSelfCapture operation: sending @escaping @isolated(any) () async throws(Failure) -> Success
 	) {
 		let timeoutOperation = { () async throws(Failure) -> Success in
-			do {
-				return try await withThrowingTaskGroup(of: Success.self) { group in
-					group.addTask {
-						try await operation()
-					}
-
-					group.addTask {
-						try await Task.sleep(for: timeout)
-						throw Failure.timedOut
-					}
-
-					guard
-						let result = try await group.next()
-					else { throw Failure.timedOut }
-					return result
-				}
-			} catch let error as Failure {
-				throw error
-			} catch {
-				fatalError(
-					"""
-					Expected error of type \(Failure.self) but got an error of \(type(of: error)). \
-					However, here's the error: \(error)
-					""")
+			if shouldUseStructuredTasks {
+				return try await Self.structuredGroupTask(timeout: timeout, operation: operation)
+			} else {
+				return try await Self.unstructuredGroupTask(timeout: timeout, operation: operation)
 			}
 		}
 
@@ -75,13 +80,127 @@ public struct TimeoutTask<Success: Sendable, Failure: TimedOutError>: Sendable, 
 		}
 	}
 
-	/// Same as `Task.detached`
+	private static func structuredGroupTask(
+		timeout: ContinuousClock.Instant.Duration,
+		@_implicitSelfCapture operation: sending @escaping @isolated(any) () async throws(Failure) -> Success
+	) async throws(Failure) -> Success {
+		do {
+			return try await withThrowingTaskGroup(of: Success.self) { group in
+				group.addTask {
+					try await operation()
+				}
+
+				group.addTask {
+					do {
+						try await Task.sleep(for: timeout)
+						throw Failure.timedOut
+					} catch is CancellationError {
+						throw Failure.timedOut
+					}
+				}
+
+				defer { group.cancelAll() }
+				guard
+					let result = try await group.next()
+				else { throw Failure.timedOut }
+				return result
+			}
+		} catch let error as Failure {
+			throw error
+		} catch is CancellationError {
+			throw .cancelled
+		} catch {
+			fatalError(
+				"""
+				Expected error of type \(Failure.self) but got an error of \(type(of: error)). \
+				However, here's the error: \(error)
+				""")
+		}
+	}
+
+	private static func unstructuredGroupTask(
+		timeout: ContinuousClock.Instant.Duration,
+		@_implicitSelfCapture operation: sending @escaping @isolated(any) () async throws(Failure) -> Success
+	) async throws(Failure) -> Success {
+		do {
+			let continuationProxy = ContinuationProxy<Success, Failure>()
+
+			let taskWrapper = Task {
+				do throws(Failure) {
+					let result = try await operation()
+					continuationProxy.resume(returning: result)
+				} catch {
+					continuationProxy.resume(throwing: error)
+				}
+			}
+
+			return try await withThrowingTaskGroup(of: Success.self) { group in
+				group.addTask {
+					try await withTaskCancellationHandler(
+						operation: {
+							try await withUnsafeThrowingContinuation { continuation in
+								continuationProxy.setContinuation(continuation)
+							}
+						},
+						onCancel: {
+							taskWrapper.cancel()
+							continuationProxy.resume(throwing: .cancelled)
+						})
+				}
+
+				group.addTask {
+					try await Task.sleep(for: timeout)
+					throw Failure.timedOut
+				}
+
+				defer { group.cancelAll() }
+				guard
+					let result = try await group.next()
+				else { throw Failure.timedOut }
+				return result
+			}
+		} catch let error as Failure {
+			throw error
+		} catch {
+			fatalError(
+				"""
+				Expected error of type \(Failure.self) but got an error of \(type(of: error)). \
+				However, here's the error: \(error)
+				""")
+		}
+	}
+
+	/// Basics are the same as `Task.detached`, but `shouldUseStructuredTasks` differs.
+	///
+	/// When `shouldUseStructuredTasks` is set to `true`, that means that the whole task tree is created with structured
+	/// tasks (although, any child `Task`s of `operation` are at your mercy). This is usually the *right* strategy,
+	/// however there are some potential exceptions.
+	///
+	/// When using code that isn't async/await aware, or maybe even some code that *is*, the `Task` won't respect the
+	/// `isCancelled` state of the `Task`. The consequence of this is that, when using structured tasks, even the
+	/// cancelled branch of the tree must finish everything before the parent task can complete. So, for example, if a
+	/// task were to `usleep()` for `10` seconds, but your timeout was set to `1` second, you would still have to
+	/// wait the entire `10` seconds before learning that your task timed out. The underlying code *will still* execute
+	/// to completion regardless of this state here, but often times you don't care and just need to update UI and
+	/// consider it a write off.
+	///
+	/// Contrarily, if the `usleep()` function were called in an unstructured child task, the timeout can trigger
+	/// immediately upon firing, informing the call site that the task timed out after the expected `1` second interval.
+	///
+	/// Of note, when `shouldUseStructuredTasks` is false, it is only the `operation` closure that is passed to the
+	/// unstructured task (and therefore any children of that `Task`), but the rest of the tree remains structured.
 	public static func detached(
 		priority: TaskPriority? = nil,
 		timeout: ContinuousClock.Instant.Duration,
+		shouldUseStructuredTasks: Bool = true,
 		operation: sending @escaping @isolated(any) () async throws(Failure) -> Success
 	) -> TimeoutTask<Success, Failure> {
-		Self.init(detached: true, priority: priority, timeout: timeout, operation: operation)
+		Self.init(
+			detached: true,
+			priority: priority,
+			timeout: timeout,
+			shouldUseStructuredTasks: shouldUseStructuredTasks,
+			operation: operation)
 	}
 
 	/// Same as `Task.cancel`
--- Tests/SwiftPizzaSnipsTests/TimeoutTaskTests.swift
+++ Tests/SwiftPizzaSnipsTests/TimeoutTaskTests.swift
@@ -3,8 +3,44 @@ import Foundation
 import SwiftPizzaSnips
 
 struct TimeoutTaskTests {
+	// This test is expected to take the whole 1 second
+	@Test(arguments: [true, false]) func asyncUnawareTimesOutStructured(detached: Bool) async throws {
+		let timerStart = Date()
+		let task = createTask(detached: detached, structured: true, timeout: 0.01) { () async throws(TestingError) -> Bool in
+			usleep(1_000_000)
+			// It is expected that this will always print, delaying the timeout.
+			print("🤬🤬🤬 (Task.isCancelled was ignored and execution continued (\(#function))")
+            throw TestingError.failed
+        }
+
+        await #expect(throws: TestingError.timedOut, performing: {
+            try await task.value
+        })
+		let timerEnd = Date()
+
+		#expect(timerEnd.timeIntervalSince(timerStart) >= 1)
+    }
+
+	// This test is expected to only take the 0.01 ±a bit timeout duration
+	@Test(arguments: [true, false]) func asyncUnawareTimesOutUnstructured(detached: Bool) async throws {
+		let task = createTask(detached: false, structured: false, timeout: 0.01) { () async throws(TestingError) -> Bool in
+			usleep(1_000_000)
+			// It is generally expected that this won't always print out as the timeout should ignore that the code is still
+			// executing and finish. However, if the *overall program* continues long enough for the `usleep` to complete,
+			// the print statement will still execute. Think of the potential secondary side effects that could happen in
+			// unpure functions passed to TimeoutTask.
+			print("🤬🤬🤬 (Task.isCancelled was ignored and execution continued (\(#function))")
+			throw TestingError.failed
+		}
+
+		await #expect(throws: TestingError.timedOut, performing: {
+			try await task.value
+		})
+	}
+
 	@Test(arguments: [true, false]) func success(detached: Bool) async throws {
-		let task = createTask(detached: detached, timeout: 3) { () async throws(TestingError) -> Bool in
+		let timerStart = Date()
+		let task = createTask(detached: detached, structured: true, timeout: 3) { () async throws(TestingError) -> Bool in
 			do {
 				try await Task.sleep(for: .seconds(0.01))
 			} catch {
@@ -15,12 +51,14 @@ struct TimeoutTaskTests {
 		}
 
 		let result = try await task.value
+		let timerEnd = Date()
 
 		#expect(result == true)
+		#expect(timerEnd.timeIntervalSince(timerStart) < 0.5)
 	}
 
 	@Test(arguments: [true, false]) func failure(detached: Bool) async throws {
-		let task = createTask(detached: detached, timeout: 5) { () async throws(TestingError) -> Void in
+		let task = createTask(detached: detached, structured: true, timeout: 5) { () async throws(TestingError) -> Void in
 			throw .expected
 		}
 
@@ -30,7 +68,7 @@ struct TimeoutTaskTests {
 	}
 
 	@Test(arguments: [true, false]) func cancel(detached: Bool) async throws {
-		let task = createTask(detached: detached, timeout: 5) { () async throws(TestingError) -> Void in
+		let task = createTask(detached: detached, structured: true, timeout: 5) { () async throws(TestingError) -> Void in
 			do {
 				try await Task.sleep(for: .seconds(2))
 			} catch {
@@ -47,14 +85,14 @@ struct TimeoutTaskTests {
 
 		let result = await task.result
 
-		#expect(throws: TestingError.expected, performing: {
+        #expect(throws: Error.self, performing: {
 			try result.get()
 		})
 		#expect(task.isCancelled == true)
 	}
 
 	@Test(arguments: [true, false]) func timeoutReached(detached: Bool) async throws {
-		let task = createTask(detached: detached, timeout: 0.2) { () async throws(TestingError) -> Void in
+		let task = createTask(detached: detached, structured: true, timeout: 0.2) { () async throws(TestingError) -> Void in
 			do {
 				try await Task.sleep(for: .seconds(2))
 			} catch {
@@ -74,13 +112,14 @@ struct TimeoutTaskTests {
 
 	private func createTask<Success: Sendable, Failure: TimedOutError>(
 		detached: Bool,
+		structured: Bool,
 		timeout: TimeInterval,
 		_ op: @escaping () async throws(Failure) -> Success
 	) -> TimeoutTask<Success, Failure> {
 		if detached {
-			TimeoutTask.detached(timeout: .seconds(timeout), operation: op)
+			TimeoutTask.detached(timeout: .seconds(timeout), shouldUseStructuredTasks: structured, operation: op)
 		} else {
-			TimeoutTask(timeout: .seconds(timeout), operation: op)
+			TimeoutTask(timeout: .seconds(timeout), shouldUseStructuredTasks: structured, operation: op)
 		}
 	}
 
